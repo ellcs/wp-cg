fd968811cc7aa5b9572324420f27d0a8a5f1538d
Alex Mantel <alexmantel93@gmail.com>
2016-09-20 15:23:01 +0200

    Add rotation node

--- src/main/java/computergraphics/framework/scenegraph/RotationNode.java
+++ src/main/java/computergraphics/framework/scenegraph/RotationNode.java
@@ -7,28 +7,26 @@
 package computergraphics.framework.scenegraph;
 
 import com.jogamp.opengl.GL2;
-
-import computergraphics.framework.math.Vector;
 import computergraphics.framework.math.Matrix;
+import computergraphics.framework.math.Vector;
 
 /**
  * Translate all child nodes.
  * 
  * @author Philipp Jenke
  */
-public class TranslationNode extends InnerNode {
+public class RotationNode extends InnerNode {
 
-  /**
-   * Translation matrix (model matrix)
-   */
-  private final Matrix translation;
+  private double angle;
+  private Matrix matrix;
 
-  public TranslationNode(Vector translation) {
-  this.translation = Matrix.createTranslationGl(translation);
+  public RotationNode(Vector vector, double angle) {
+    this.angle = angle;
+    this.matrix = Matrix.getRotationMatrix3(vector, angle);
   }
 
   public void traverse(GL2 gl, RenderMode mode, Matrix modelMatrix) {
-    super.traverse(gl, mode, translation.multiply(modelMatrix));
+      super.traverse(gl, mode, matrix.multiply(modelMatrix));
   }
 
   public void timerTick(int counter) {
